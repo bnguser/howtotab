f627913ebd95fe3392f804aaf842704c3d6c5630
bnguser <batngoal@gmail.com>
2019-02-04 15:22:27 +0530

    check node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,21 @@
 (function(){
     "use strict";
 
-
+    var EXPECTED_NODE_VERSION ="v0.12.4";
     desc("Default Build");
-    task("default",function(){
+    task("default", ["version"],function(){
         console.log("\n\nBuild Ok");
+    });
+    desc("Check Node Version");
+    task("version",function(){
+        console.log("Check node Version : .");
+
+        var actualVersion = process.version;
+        if(actualVersion !== EXPECTED_NODE_VERSION){
+            fail("Incorrect Node version : expected " + EXPECTED_NODE_VERSION + "but was" + actualVersion);
+
+        }
+
     });
 
 }());
