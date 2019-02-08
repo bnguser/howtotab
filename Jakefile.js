cb164cda74689471493fb22ad6438118fc7825fd
bnguser <batngoal@gmail.com>
2019-02-08 22:54:47 +0530

    creating placeholder in index.html

--- Jakefile.js
+++ Jakefile.js
@@ -1,3 +1,6 @@
+/*globals desc : false, task:false, complete : false, fail : false */
+
+
 (function(){
     "use strict";
     var semver = require("semver");
@@ -27,12 +30,31 @@
 
         jshint.checkFiles({
             files: "Jakefile.js",
-            options:{},
+            options:{
+                bitwise : true,
+                eqeqeq : true,
+                futurehostile : true,
+                forin : true,
+                freeze : true,
+                latedef : "nofunc",
+                noarg: true,
+                nocomma : true,
+                nonbsp : true,
+                nonew : true,
+                strict : true,
+                undef : true,
+
+
+                node : true,
+                browser: true,
+
+            },
             globals:{}
         },complete ,fail);
 
 
         // jake.exec("node node_modules/jshint/bin/jshint jakefile.js",{interactive : true }, complete);
     },{async:true});
+
 
 }());
