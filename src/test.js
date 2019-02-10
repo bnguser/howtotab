f3b4209525f7abb3883f9b2035c0c59bd606f5cf
bnguser <batngoal@gmail.com>
2019-02-10 15:23:33 +0530

    installed karma

--- src/test.js
+++ src/test.js
@@ -2,18 +2,21 @@
     "use strict";
 
 
-    var assert = require("chai").assert;
-
     describe("Addition", function(){
        it("add positive numbers", function(){
-           // basic addition
-           assert.equal(add(3,4), 7);
+
+           assertEqual(add(3,4), 7);
        });
        it("use IEEE 754 floating point number", function(){
-           // floating point addition
 
-           assert.equal(add(0.1,0.2), 0.30000000000000004);
+
+           assertEqual(add(0.1,0.2), 0.30000000000000004);
        });
+
+       function assretEqual(actual,expected){
+           if(actual !== expected)   throw new Error("Expected" + expected + ", but was " + actual);
+       };
+
     });
 
 
