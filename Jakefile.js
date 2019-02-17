3450fe1b7b2807c464912f7e71f1a6c1796bf0b4
bnguser <batngoal@gmail.com>
2019-02-17 22:10:56 +0530

    'clean' and 'build' tasks

--- Jakefile.js
+++ Jakefile.js
@@ -8,6 +8,7 @@
     var karma = require("simplebuild-karma");
 
     var KARMA_CONF = "karma.conf.js";
+   var DIST_DIR ="generated/dist";
     
     //*** General tasks
 
@@ -25,10 +26,14 @@
     });
 
     desc("Run localhost server");
-    task("run", function(){
-        jake.exec("node node_modules/http-server/bin/http-server src",  {interactive : true}, complete);
+    task("run",["build"],  function(){
+        jake.exec("node node_modules/http-server/bin/http-server " + DIST_DIR ,  {interactive : true}, complete);
     });
 
+    desc("Erase all generated files");
+    task("clean", function(){
+        console.log("Earse generated files");
+    });
 
 
     //*** Supporting tasks
@@ -71,6 +76,12 @@
            strict: !process.env.loose
        }, complete, fail);
     },{async : true});
+
+    desc("Building Distribution Directory");
+    task("build",[DIST_DIR], function(){
+        console.log("Building Dist Directory : ");
+    });
+    directory(DIST_DIR);
 
     function lintOptions(){
         return {
